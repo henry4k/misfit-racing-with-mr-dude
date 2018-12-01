63dbebd2a74941d534e8cda58519f3cbd9f0478c
Dominik <dominik.reitz@yahoo.de>
2018-12-01 21:38:13 +0100

    graphic changes

--- awesomename/Assets/Scripts/Game.cs
+++ awesomename/Assets/Scripts/Game.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
-using DG.Tweening;
 using UnityEngine;
 using BayatGames.SaveGameFree;
 public class Game
